7e5e82310c6a1e799557b926f002b2ffb717c0b2
ioannis-mylonas <ioannis.mylonas@gmail.com>
2021-05-26 17:16:33 -0300

    Piloto: Ulysses, Copiloto: Ioannis

---
+++ src/main/kotlin/br/com/zup/edu/cadastraAutor/AutorRepository.kt
@@ -0,0 +1,9 @@
+package br.com.zup.edu.cadastraAutor
+
+import io.micronaut.data.annotation.Repository
+import io.micronaut.data.jpa.repository.JpaRepository
+
+@Repository
+interface AutorRepository: JpaRepository<Autor, Long> {
+    fun existsByEmail(email: String)
+}
--- src/main/kotlin/br/com/zup/edu/cadastraAutor/CadastraAutorEndpoint.kt
+++ src/main/kotlin/br/com/zup/edu/cadastraAutor/CadastraAutorEndpoint.kt
@@ -4,16 +4,23 @@ import br.com.zup.edu.CadastraAutorServiceGrpc
 import br.com.zup.edu.NovoAutorRequest
 import br.com.zup.edu.NovoAutorResponse
 import io.grpc.stub.StreamObserver
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class CadastraAutorEndpoint : CadastraAutorServiceGrpc.CadastraAutorServiceImplBase() {
 
+    @Inject lateinit var repository: AutorRepository
+
     override fun cadastraAutor(
-        request: NovoAutorRequest?,
-        responseObserver: StreamObserver<NovoAutorResponse>?
+        request: NovoAutorRequest,
+        responseObserver: StreamObserver<NovoAutorResponse>
     ) {
 
-        val autor = request?.toModel()
+        val autor = request.toModel()
+        repository.save(autor)
+
+        responseObserver.onNext(NovoAutorResponse.newBuilder().setId(autor.id.toString()).build())
+        responseObserver.onCompleted()
     }
 }
